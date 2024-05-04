c4f8d74ef9a8a38fd326c5b1de130dc334a98968
JujuDeFruits <julien.franchet@supinfo.com>
2024-05-04 11:02:58 +0200

    update 03/05

--- main.py
+++ main.py
@@ -15,23 +15,32 @@ class Algo():
     def __init__(self):
         self.tableau=[]
     
-    def rotation(self):
-        tab = self.tableau
-        for i in range(len(self.tableau)):
-            for j in range(len(self.tableau)):
-                tab[5-j][i], self.tableau[i][j] = self.tableau[i][j], tab[5-j][i]
+    def rotation_droite(self):
+        n = len(self.tableau)
+        tab = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                tab[j][n-1-i] = self.tableau[i][j]
         self.tableau = tab
+
     
     def init_tableau(self, cote):
-        if len(self.tableau) % 2 == 0:
-            for _ in range(cote):
+        for _ in range(cote):
                 self.tableau.append([0] * cote)
+        if len(self.tableau) % 2 == 0:
             for i in range((len(self.tableau))//2):
                 for j in range((len(self.tableau))//2):
-                    self.tableau[i][j]=1
                     rand=randint(1,4)
+                    self.tableau[i][j]=1
                     for _ in range(rand):
-                        self.rotation()
+                        self.rotation_droite()
+        else :
+            for i in range(((len(self.tableau))//2)+1):
+                for j in range((len(self.tableau))//2):
+                    rand=randint(1,4)
+                    self.tableau[i][j]=1
+                    for _ in range(rand):
+                        self.rotation_droite()
                     
     def add_lettre(self):
         pass
@@ -44,5 +53,4 @@ def main():
     jeu.init_tableau(cote)
     for i in range(len(jeu.tableau)):
         print(jeu.tableau[i])
-    
 main()
